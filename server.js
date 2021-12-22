824484bb17bcca6c52908ce9d6d2f979b3ecff0e
Joey Zhu <joey.j.zhu@berkeley.edu>
2021-12-21 22:44:34 -0800

    updated

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index') // Use this path relative to our cwd to get index and open application
+const authorRouter = require('./routes/authors') // Use this path relative to our cwd to get index and open application
+
 // Also need a way to get information from this index to run it
 
 app.set('view engine', 'ejs') // Express has an attribute called view engine
@@ -23,7 +25,10 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+// Tell app to use routers, with directories located as input
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 
 app.listen(process.env.PORT || 3000) // Server sets port, not us
 
